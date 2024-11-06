9a1d30ae09741df3db8f9ddcde322ddfa0b867c2
Ezekiel <gituraezekiel@gmail.com>
2024-11-06 12:35:43 +0300

    new responsive code

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+// toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sectctions.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAtttribute;
-
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*m' + id + ']').classList.add('active')
-            })
-        }
-    })
-}
 
 menuIcon.onClick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+letnavlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelectorAll('header nav a[href*=' + id +']').classList.add('active');
+            });
+        }
+
+    });
+
+    //sticky header
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar links (scroll)
 }
